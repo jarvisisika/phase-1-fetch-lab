5eefcfaa04cd4c7c58bea928789549a4204bf09c
jarvisisika <jarvis.isika@student.moringaschool.com>
2023-03-30 06:51:09 +0300

    competed assignment

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
+
+
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then(response => response.json())
+    .then(data => renderBooks(data))
+    .catch(error => console.error('Error fetching books:', error));
 }
 
-function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
+
+function renderBooks(data) {
+  const bookList = document.querySelector('#book-list');
+
+  data.forEach(book => {
+    const listItem = document.createElement('li');
+    listItem.textContent = book.name;
+    bookList.appendChild(listItem);
   });
 }
-
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
